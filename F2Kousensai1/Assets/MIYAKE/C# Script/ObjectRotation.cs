73af0694ffe3bdd827a6e2bd810446e8874355a9
f21012 <f21012@osaka-pct.ac.jp>
2022-11-01 00:06:05 +0900

    スピン作成

--- F2Kousensai1/Assets/MIYAKE/C# Script/ObjectRotation.cs
+++ F2Kousensai1/Assets/MIYAKE/C# Script/ObjectRotation.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class ObjectRotation : MonoBehaviour
 {
-    // 中心点
-    // [SerializeField] private Vector3 _center = (GameObject.Find("Banana")).GetComponent<Renderer>().bounds.center;
-    [SerializeField] private Vector3 _center = this.GetComponent<Renderer>().bounds.center;
-
-    // 回転軸
-    [SerializeField] private Vector3 _axis = Vector3.up;
-
-    // 円運動周期
-    [SerializeField] private float _period = 2;
+    Renderer rend;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        // 中心点
+        // A sphere that fully encloses the bounding box.
+        // Vector3 center = rend.bounds.center;
+
+        // // 回転軸
+        // private Vector3 _axis = Vector3.up;
+
+        // // 円運動周期
+        // private float _period = 3;
     }
 
     // Update is called once per frame
@@ -36,11 +37,22 @@ public class ObjectRotation : MonoBehaviour
         // Vector3 direction = other.gameObject.GetComponent<Rigidbody>().transform.position - transform.position;
         // other.gameObject.GetComponent<Rigidbody>().AddForceAtPosition(direction.normalized, transform.position);
         // other.gameObject.GetComponent<Transform>().transform.Rotate(0, 180, 0);
-        // 中心点centerの周りを、軸axisで、period周期で円運動
-        other.gameObject.GetComponent<Transform>().transform.RotateAround(
-            (GameObject.Find("Banana")).GetComponent<Renderer>().bounds.center,
-            _axis,
-            360 / _period * Time.deltaTime
-        );
+        Vector3 center = rend.bounds.center;
+
+        // 回転軸
+        Vector3 _axis = Vector3.up;
+
+        // 円運動周期
+        float _period = 1;
+        
+        for(int i=0; i<10; i++){
+            // 中心点centerの周りを、軸axisで、period周期で円運動
+            other.gameObject.GetComponent<Transform>().transform.RotateAround(
+                center,
+                _axis,
+                360 / _period * Time.deltaTime
+            );
+        }
+        
     }
 }
