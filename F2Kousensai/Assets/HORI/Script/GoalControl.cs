83fe7e570edf73d57fa591c5878d95650fff3900
horikouki <f21011@osaka-pct.ac.jp>
2022-11-01 15:57:23 +0900

    a
    細かい調整

--- F2Kousensai/Assets/HORI/Script/GoalControl.cs
+++ F2Kousensai/Assets/HORI/Script/GoalControl.cs
@@ -7,8 +7,8 @@ public class GoalControl : MonoBehaviour
 {
 
     //public GameObject MainText;
-    public PlayerController player;
-    public GameObject playerP;
+    PlayerController player;
+    GameObject player1,player2,player3,player4;
 
     // Start is called before the first frame update
     void Start()
@@ -25,13 +25,38 @@ public class GoalControl : MonoBehaviour
 
     void OnTriggerEnter(Collider collider) //物体がすり抜けた時に呼び出す
     {
-        playerP = GameObject.FindWithTag("Player1");
+        player1 = GameObject.FindWithTag("Player1");
+        player2 = GameObject.FindWithTag("Player2");
+        player3 = GameObject.FindWithTag("Player3");
+        player4 = GameObject.FindWithTag("Player4");
+
         if (collider.gameObject.tag == "Player1")
         {
             Debug.Log("呼び出し");
             //player.LapNum();
             //PlayerController.instance.LapNum();
-            playerP.GetComponent<PlayerController>().LapNum();
+            player1.GetComponent<PlayerController>().LapNum();
+        }
+        else if (collider.gameObject.tag == "Player2")
+        {
+            Debug.Log("呼び出し2");
+            //player.LapNum();
+            //PlayerController.instance.LapNum();
+            player2.GetComponent<PlayerController>().LapNum();
+        }
+        else if (collider.gameObject.tag == "Player3")
+        {
+            Debug.Log("呼び出し3");
+            //player.LapNum();
+            //PlayerController.instance.LapNum();
+            player3.GetComponent<PlayerController>().LapNum();
+        }
+        else if (collider.gameObject.tag == "Player4")
+        {
+            Debug.Log("呼び出し4");
+            //player.LapNum();
+            //PlayerController.instance.LapNum();
+            player4.GetComponent<PlayerController>().LapNum();
         }
         //通過をタグ（Player1みたいなの）で判定して、通過したプレイヤーの関数を呼び出して関数内で周回数や通過場所などを管理？
     }
--- F2Kousensai/Assets/HORI/Script/RoadControl.cs
+++ F2Kousensai/Assets/HORI/Script/RoadControl.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class RoadControl : MonoBehaviour
 {
     //public GameObject MainText;
-    public PlayerController player;
+    PlayerController player;
+    GameObject player1, player2, player3, player4;
 
 
     // Start is called before the first frame update
@@ -26,10 +27,34 @@ public class RoadControl : MonoBehaviour
 
         if (collider.gameObject.tag == "Player1")
         {
-            Debug.Log("呼び出し2");
-            player.RoadNum();
-            //MainText.SetActive(true); //事前にセットしたテキストを表示
+            Debug.Log("チェック1");
+            //player.LapNum();
+            //PlayerController.instance.LapNum();
+            player1.GetComponent<PlayerController>().RoadNum();
         }
+        else if (collider.gameObject.tag == "Player2")
+        {
+            Debug.Log("チェック2");
+            //player.LapNum();
+            //PlayerController.instance.LapNum();
+            player2.GetComponent<PlayerController>().RoadNum();
+        }
+        else if (collider.gameObject.tag == "Player3")
+        {
+            Debug.Log("チェック3");
+            //player.LapNum();
+            //PlayerController.instance.LapNum();
+            player3.GetComponent<PlayerController>().RoadNum();
+        }
+        else if (collider.gameObject.tag == "Player4")
+        {
+            Debug.Log("チェック4");
+            //player.LapNum();
+            //PlayerController.instance.LapNum();
+            player4.GetComponent<PlayerController>().RoadNum();
+        }
+
+
         //通過をタグ（Player1みたいなの）で判定して、通過したプレイヤーの関数を呼び出して関数内で周回数や通過場所などを管理？
     }
 }
